3e8aba553167e6de56d28e2ecd26e0e2548448d4
Gabriel758Eduardo <eduardosantos9406@gmail.com>
2020-08-27 23:13:39 -0300

    Arquivo de interface desenvolvido

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<html>
-
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title> Projeto - Indice de Massa Corporal </title>
+	</head>
+	<body>
+		<form action="processa.php" method="POST">
+			<label>Digite seu peso:</label>
+			<input type="number" name="peso">
+			<br/>
+			
+			<label>Digite sua altura:</label>
+			<input type="number" name="altura">
+			<br/>
+			
+			<input type="submit" value="Calcular" />
+			
+		</form>
+	</body>
+</html>
 </html>
